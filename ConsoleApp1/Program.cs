9031413134e2def34f389b25bfb1c6380dfe045e
Dragon2517 <evseev.vasya@yandex.ru>
2022-03-16 08:45:45 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Turtle.Speed = 9;
+            //Turtle.TurnRight();
+            int t = 0;
+
+            while (t < 6)
+            {
+                Turtle.Move(40);
+                Turtle.Turn(60);
+
+                t++;
+                
+
+            }
+            Turtle.Move(20);
+            Turtle.TurnLeft();
+            t = 0;
+            while (t<3)
+            {
+                Turtle.Move(20);
+                Turtle.TurnRight();
+
+                Turtle.Move(20);
+                Turtle.TurnLeft();
+
+                Turtle.Move(20);
+                Turtle.TurnLeft();
+
+                Turtle.Move(20);
+                Turtle.TurnRight();
+                t++;
+
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void WriteT(int T)
+        {
+
+
+        }
+        static void Main(string[] args)
+        {
+            Turtle.Speed = 10;
+            Turtle.X = 200;
+            Turtle.Y = 200;
+
+
+            
+        }
+    }
+}
