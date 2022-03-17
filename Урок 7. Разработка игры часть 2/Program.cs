1204f68bc0cfc39179e1c133b5ee62bee4bb42bc
Dragon2517 <evseev.vasya@yandex.ru>
2022-03-17 10:12:19 +0300

    Урок 7. Разработка игры: часть 2

---
+++ Урок 7. Разработка игры часть 2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+
+namespace Урок_7.Разработка_игры_часть_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GraphicsWindow.KeyDown += GraphicsWindow_KeyDown;
+            Turtle.PenUp();
+
+            int P = 50;
+            int Ex = 200, Ey = 200;
+            GraphicsWindow.BrushColor = "Red";
+            var eat = Shapes.AddRectangle(P, P);
+            Shapes.Move(eat, Ex, Ey);
+
+
+            Random random = new Random();
+
+
+            while (true)
+            {
+                Turtle.Move(10);
+
+                if (Turtle.X > Ex && Turtle.X < Ex + P && Turtle.Y > Ey && Turtle.Y < Ey + P)
+                {
+                    Ex=random.Next(0,GraphicsWindow.Width-P);
+                    Ey=random.Next(0,GraphicsWindow.Height-P);
+                    //Ex = Ex + P;
+                    //Turtle.X = 250;
+                    //Turtle.Y = 200;
+
+                    Shapes.Move(eat, Ex, Ey);
+
+                }
+            }
+
+
+        }
+
+
+
+        private static void GraphicsWindow_KeyDown()
+        {
+            if (GraphicsWindow.LastKey == "Up")
+            {
+                Turtle.Angle = 0;
+            }
+            else if (GraphicsWindow.LastKey == "Right")
+            {
+                Turtle.Angle = 90;
+            }
+            else if (GraphicsWindow.LastKey == "Down")
+            {
+                Turtle.Angle = 180;
+            }
+            else if (GraphicsWindow.LastKey == "Left")
+            {
+                Turtle.Angle = 270;
+            }
+        }
+    }
+}
---
+++ Урок 7. Разработка игры часть 2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Урок 7. Разработка игры часть 2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Урок 7. Разработка игры часть 2")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("13ff7b2f-a6ec-46c9-af9f-f538315b733c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
