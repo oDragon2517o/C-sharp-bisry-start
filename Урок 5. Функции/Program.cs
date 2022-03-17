6877f2ee5fde035438e265136c730ee369d758ba
Dragon2517 <evseev.vasya@yandex.ru>
2022-03-17 08:52:49 +0300

    Саша

--- Урок 5. Функции/Program.cs
+++ Урок 5. Функции/Program.cs
@@ -50,6 +50,48 @@ namespace Урок_5.Функции
 
         }
 
+        static void WriteA(double size)
+        {
+            Turtle.Angle = 0;
+            Turtle.Turn(15);
+            Turtle.Move(size);
+            Turtle.Turn(180-2*15);
+            Turtle.Move(size);
+            Turtle.Turn(180);
+            Turtle.Move(size/2);
+            Turtle.Turn(-(90- 15));
+            Turtle.Move(size / 4);
+        }
+        static void WriteC(double size)
+        {
+            Turtle.Angle = 0;
+            Turtle.TurnLeft();
+            for (int i = 0; i < 3; i++)
+            {
+                
+                Turtle.Move(size);
+                Turtle.TurnRight();
+
+            }
+        }
+        static void WriteSHA(double size)
+        {
+
+            Turtle.Angle = 0;
+            Turtle.Move(size);
+            Turtle.Move(-size);
+            Turtle.TurnRight();
+            Turtle.Move(size/2);
+            Turtle.TurnLeft();
+            Turtle.Move(size);
+            Turtle.Move(-size);
+            Turtle.TurnRight();
+            Turtle.Move(size/2);
+            Turtle.TurnLeft();
+            Turtle.Move(size);
+
+
+        }
 
         static void Main(string[] args)
         {
@@ -58,7 +100,7 @@ namespace Урок_5.Функции
             Turtle.X = 200;
             Turtle.Y = 200;
             WriteT(20);
-            
+
             Turtle.X = 250;
             Turtle.Y = 200;
             WriteO(30);
@@ -72,7 +114,22 @@ namespace Урок_5.Функции
             Turtle.Y = 200;
             WriteT(10);
 
+            Turtle.X = 200;
+            Turtle.Y = 300;
+            WriteC(50);
 
+            Turtle.X = 225;
+            Turtle.Y = 300;
+            WriteA(30);
+
+
+            Turtle.X = 250;
+            Turtle.Y = 300;
+            WriteSHA(30);
+
+            Turtle.X = 300;
+            Turtle.Y = 300;
+            WriteA(30);
 
         }
     }
